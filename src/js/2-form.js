fa097e005f193b2b84ff88aecc4f0afeb5990d81
KiraKostina <kostina.kira@gmail.com>
2024-03-09 23:59:24 +0200

    form.js

---
+++ src/js/2-form.js
@@ -0,0 +1,38 @@
+const form = document.querySelector('.feedback-form');
+    
+form.addEventListener("input", handlerInput);
+function handlerInput(event) {
+   const email = form.elements.email.value.trim();
+const message = form.elements.message.value.trim();
+    const data = JSON.stringify({ email, message});
+    localStorage.setItem("feedback-form-state", data);
+}
+ 
+const savedData = localStorage.getItem("feedback-form-state");
+if (savedData) {
+    const info = JSON.parse(savedData);
+form.elements.email.value = info.email;
+form.elements.message.value = info.message;
+}
+
+
+
+
+form.addEventListener('submit', handlerSubmit);
+function handlerSubmit(event) {
+    event.preventDefault();
+
+    const email = form.elements.email.value.trim();
+    const message = form.elements.message.value.trim();
+
+    if (email === "" || message === "") {
+        return alert("All form fields must be filled in");
+    }
+
+    const data = JSON.stringify({ email, message });
+    localStorage.removeItem("feedback-form-state");
+    
+    console.log({email, message});
+
+form.reset();
+ }
